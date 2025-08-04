c7f410a9f883b21b90ca93a843c949aa70cfd680
ancu-98 <jdsebastianb30@hotmail.com>
2025-08-03 20:30:22 -0500

    adding Django Admon introduction

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Question
 
 # Register your models here.
+
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "The world's slicket Admin Panel"
+admin.site.index_title = "New Title"
+
+admin.site.register(Question)
