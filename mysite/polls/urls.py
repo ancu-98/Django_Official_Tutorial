4d357993680cace98986ccf23f78492e6b14059d
ancu-98 <jdsebastianb30@hotmail.com>
2025-08-04 20:10:14 -0500

    adding urls views paths

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"), #localhost:8000/polls/
+    # ex: /polls/
+    path("", views.index, name="index"),  #localhost:8000/polls/
+    # ex: specifics/polls/5/
+    # the 'name' value as called by the {% url %} template tag on index.html template
+    # added the word 'specifics'
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
