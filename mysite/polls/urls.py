c03960f39519d321da5712edc6191ddd297cc14d
ancu-98 <jdsebastianb30@hotmail.com>
2025-08-08 20:06:35 -0500

    update index, detail and results to generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,13 +6,13 @@ app_name ="polls"
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),  #localhost:8000/polls/
+    path("", views.IndexView.as_view(), name="index"),  #localhost:8000/polls/
     # ex: specifics/polls/5/
     # the 'name' value as called by the {% url %} template tag on index.html template
     # added the word 'specifics'
-    path("specifics/<int:question_id>/", views.detail, name="detail"),
+    path("specifics/<int:pk>/", views.DetailView.as_view(), name="detail"),
     # ex: /polls/5/results/
-    path("specifics/<int:question_id>/results/", views.results, name="results"),
+    path("specifics/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # ex: /polls/5/vote/
     path("specifics/<int:question_id>/vote/", views.vote, name="vote"),
 ]
