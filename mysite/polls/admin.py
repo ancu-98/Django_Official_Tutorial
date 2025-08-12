debe9d164751c7bc9529d27e05c5dfa5c79361c9
ancu-98 <jdsebastianb30@hotmail.com>
2025-08-12 16:36:29 -0500

    modifying admin views

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 
@@ -7,4 +7,19 @@ admin.site.site_header = "Polls Admin"
 admin.site.site_title = "The world's slicket Admin Panel"
 admin.site.index_title = "New Title"
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
