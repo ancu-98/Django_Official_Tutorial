8963c514149ae1fbd42e622a20da36a0cd13e1e6
ancu-98 <jdsebastianb30@hotmail.com>
2025-08-13 15:21:32 -0500

    adding django-debug-toolbar configs

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("polls/", include("polls.urls")),  #localhost:8000/polls/
     path('admin/', admin.site.urls),        #localhost:8000/admin/
-]
+] + debug_toolbar_urls()
